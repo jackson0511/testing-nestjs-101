9b2fc46ecdf7a338373b24bc6c3d23d99e5e5549
Nguyen Duc Huy <nguyenduchuy1999@gmail.com>
2022-05-29 22:26:02 +0700

    Create Patients Resource with GraphQL (schema first) and CRUD entry points

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { UsersModule } from './users/users.module';
 import { ClientsModule } from './clients/clients.module';
 import { CustomersModule } from './customers/customers.module';
 import { DoctorsModule } from './doctors/doctors.module';
+import { PatientsModule } from './patients/patients.module';
 
 @Module({
-  imports: [UsersModule, ClientsModule, CustomersModule, DoctorsModule],
+  imports: [UsersModule, ClientsModule, CustomersModule, DoctorsModule, PatientsModule],
   controllers: [AppController],
   providers: [AppService],
 })
