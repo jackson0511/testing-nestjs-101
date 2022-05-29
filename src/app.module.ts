937bef0189343b0d85ec72ef010c100b1528a876
Nguyen Duc Huy <nguyenduchuy1999@gmail.com>
2022-05-29 22:24:24 +0700

    Create Clients Resource with GraphQL (code first) and CRUD entry points

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ClientsModule } from './clients/clients.module';
+import { CustomersModule } from './customers/customers.module';
 
 @Module({
-  imports: [UsersModule, ClientsModule],
+  imports: [UsersModule, ClientsModule, CustomersModule],
   controllers: [AppController],
   providers: [AppService],
 })
